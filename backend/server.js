593451513b1de51e71f91d2cd20eff6d25db66a9
ashanhq <20064191@mydbs.ie>
2025-11-29 14:35:22 +0000

    initialize Express server in server.js

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Server running on port 5000")
+);
