de3e4aac124ede8f38999318a8694a039fd5b71a
ashanhq <20064191@mydbs.ie>
2025-12-01 01:02:53 +0000

    Add form submit event listener

--- frontend/src/assest/add.js
+++ frontend/src/assest/add.js
@@ -1,1 +1,13 @@
 const API_URL = "http://localhost:5000/api/properties";
+
+document.querySelector("#addForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const data = {
+    price: document.querySelector("#price").value,
+    type: document.querySelector("#type").value,
+    location: document.querySelector("#location").value,
+    beds: Number(document.querySelector("#beds").value),
+    baths: Number(document.querySelector("#baths").value),
+    //image: document.querySelector("#image").value 
+  };
