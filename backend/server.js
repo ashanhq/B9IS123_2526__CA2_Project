29ca3e9cf36cfe5087c998a5b253cf0fa4c13988
ashanhq <20064191@mydbs.ie>
2025-12-01 00:56:07 +0000

    Register the Routes

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB
 connectDB();
+
+// REGISTER ROUTES
+const propertyRoutes = require("./routes/propertyRoutes");
+app.use("/api/properties", propertyRoutes);
 
 app.listen(process.env.PORT || 5000, () =>
   console.log("Server running on port 5000")
