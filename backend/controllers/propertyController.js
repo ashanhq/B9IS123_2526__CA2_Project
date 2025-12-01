7c2754ab624ca9bf1c1a1e5e8637cd549e5d48f1
ashanhq <20064191@mydbs.ie>
2025-12-01 01:16:31 +0000

    Add controller logic for property API

---
+++ backend/controllers/propertyController.js
@@ -0,0 +1,26 @@
+ const Property = require("../models/Property");
+
+exports.getAll = async (req, res) => {
+  const data = await Property.find();
+  res.json(data);
+};
+
+exports.getOne = async (req, res) => {
+  const item = await Property.findById(req.params.id);
+  res.json(item);
+};
+
+exports.create = async (req, res) => {
+  await Property.create(req.body);
+  res.json({ message: "Property added" });
+};
+
+exports.update = async (req, res) => {
+  await Property.findByIdAndUpdate(req.params.id, req.body);
+  res.json({ message: "Property updated" });
+};
+
+exports.delete = async (req, res) => {
+  await Property.findByIdAndDelete(req.params.id);
+  res.json({ message: "Property deleted" });
+};
